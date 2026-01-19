64523c4b3aa92f0180b5a44fedddab06ae7f0c71
Ricardo <ricarditti@proton.me>
2026-01-19 11:17:17 +0000

    Ficha 3 veiculo

--- FT3/veiculo.php
+++ FT3/veiculo.php
@@ -8,7 +8,26 @@
     <?php
     abstract class Veiculo
     {
+        protected $placa;
+        protected $cor;
         abstract protected function mover(); 
+    }
+
+    class Bicicleta extends Veiculo
+    {
+         public function __construct($placa, $cor){
+            $this->placa = $cor;
+            $this->salario = $cor;
+        }  
+        protected function mover(){
+            return "A Bicicleta está a mover-se";
+        }
+    }
+    class Carro extends Veiculo
+    {
+        protected function mover(){
+            return "O Casrro está a mover-se";
+        }
     }
     ?>  
 </body>
